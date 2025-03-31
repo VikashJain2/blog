b6d7ee34866c539a02f64c0cf52ff8588bd7c716
VikashJain2 <vikkijain991@gmail.com>
2025-04-01 00:31:09 +0530

    refactor multer configuration to create 'uploads' directory if it doesn't exist

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,9 +1,19 @@
 import multer from "multer";
 import fs from "fs";
+import path from "path";
+
+const uploadDir = "uploads";
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
-  destination: "uploads",
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
   filename: (req, file, cb) => {
-    return cb(null, `${Date.now()}${file.originalname}`);
+    cb(null, `${Date.now()}${file.originalname}`);
   },
 });
 
